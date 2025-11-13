64e97c6ec30ead198c967f7f1fd041b867ca0130
Rutger Pronk <rutger.pronk11@gmail.com>
2025-11-13 11:39:33 +0100

    Implement RGBLED

--- src/RgbLed.cpp
+++ src/RgbLed.cpp
@@ -1,17 +1,46 @@
 #include "RgbLed.h"
+#include <Arduino.h>
 
 RgbLed::RgbLed(int redPin, int greenPin, int bluePin) 
     : redPin(redPin), greenPin(greenPin), bluePin(bluePin) {
+    // Initialize pins as outputs
+    pinMode(redPin, OUTPUT);
+    pinMode(greenPin, OUTPUT);
+    pinMode(bluePin, OUTPUT);
+    
+    Serial.println("RgbLed initialized");
+    Serial.print("Red Pin: ");
+    Serial.print(redPin);
+    Serial.print(", Green Pin: ");
+    Serial.print(greenPin);
+    Serial.print(", Blue Pin: ");
+    Serial.println(bluePin);
 }
 
 void RgbLed::on() {
-    // Code to turn on the RGB LED
+    Serial.println("RGB LED: ON (White)");
+    analogWrite(redPin, 255);
+    analogWrite(greenPin, 255);
+    analogWrite(bluePin, 255);
 }
 
 void RgbLed::off() {
-    // Code to turn off the RGB LED
+    Serial.println("RGB LED: OFF");
+    analogWrite(redPin, 0);
+    analogWrite(greenPin, 0);
+    analogWrite(bluePin, 0);
 }
 
 void RgbLed::set_color(int red, int green, int blue) {
-    // Code to set the RGB LED color
+    Serial.print("RGB LED: Setting color to (");
+    Serial.print(red);
+    Serial.print(", ");
+    Serial.print(green);
+    Serial.print(", ");
+    Serial.print(blue);
+    Serial.println(")");
+    
+    analogWrite(redPin, red);
+    analogWrite(greenPin, green);
+    analogWrite(bluePin, blue);
 }
