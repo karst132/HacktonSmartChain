228f470a526bda6e3f6a35826a27cfa0b993598b
Rutger Pronk <rutger.pronk11@gmail.com>
2025-11-13 11:03:12 +0100

    Setup main.cpp to initialize components and relationStateFacade

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,29 @@
 #include <Arduino.h>
 #include "RelationStateFacade.h"
-#include "RelationMatchContext.h"
-#include "states/SetupState.h"
-#include "states/ReadyState.h"
+#include "RgbLed.h"
+#include "Button.h"
+
+// Pin definitions
+const int RGB_RED_PIN = 9;
+const int RGB_GREEN_PIN = 10;
+const int RGB_BLUE_PIN = 11;
+const int BUTTON_PIN = 12;
+
+// Global instances
+IRgbLed* rgbLed = new RgbLed(RGB_RED_PIN, RGB_GREEN_PIN, RGB_BLUE_PIN);
+IButton* button = new Button(BUTTON_PIN);
+RelationStateFacade* facade = nullptr;
 
 void setup() {
-    // Setup code here
+    Serial.begin(115200);
+    
+    // Initialize the facade with interface pointers
+    facade = new RelationStateFacade(rgbLed, button);
 }
 
 void loop() {
-    // Main loop code here
+    Serial.println("Main loop running...");
+    if (facade != nullptr) {
+        facade->loop();
+    }
 }
